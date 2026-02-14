1ed650cb2445cdcfa3bf48db0b4965d16e034910
Siva G <sivaraman.g@gmail.com>
2026-02-14 15:30:31 -0800

    More test fixes

--- module_4/src/app.py
+++ module_4/src/app.py
@@ -155,5 +155,5 @@ def pull_data():
     except Exception as e:
         return {"error": str(e)}, 500
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     app.run(debug=True)
